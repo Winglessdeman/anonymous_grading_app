000de5457bef67eb3508fcaa524f158fba02f1b1
Winglessdeman <shristipokharel5@gmail.com>
2024-02-29 02:32:14 -0500

    java, res, manifest upload
    This upload consists of files under the app such as manifest, java, and res. Gradel script is not included

---
+++ java/com/example/anonymous_grading/InsideCourse.java
@@ -0,0 +1,16 @@
+package com.example.anonymous_grading;
+
+import static com.example.anonymous_grading.R.*;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InsideCourse extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(layout.activity_inside_course);
+    }
+}
